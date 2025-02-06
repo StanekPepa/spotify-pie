1b9351baf56aaf4ecabad99cee11764e54ef339f
Josef Staněk <stanekpepa47@gmail.com>
2025-02-06 11:38:32 +0100

    snad funguje na desktopu at home

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,6 +2,6 @@
     <div class="login">
         <h1>Login to Spotify Pie</h1>
         <p>Log in to analyze your Spotify listening habits</p>
-        <button @click="login" class="p-1.5 bg-primary">Login with Spotify</button>
+        <button class="p-1.5 bg-primary rounded-2xl">Login with Spotify</button>
     </div>
 </template>
